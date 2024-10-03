1ccd29559db89575e9edbc20ac0d1f692f01fae2
Aleksandr Domanskiy <spirit412@gmail.com>
2024-10-03 15:48:45 +0300

    1.0.0

---
+++ src/CoreServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Svr\Core;
+
+use Illuminate\Support\ServiceProvider;
+
+class CoreServiceProvider extends ServiceProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function boot()
+    {
+
+        if ($this->app->runningInConsole()) {
+            $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+            $this->publishes([__DIR__.'/../resources/lang' => resource_path('lang')], 'open-admin-lang');
+        }
+
+        RawManager::boot();
+    }
+
+}
