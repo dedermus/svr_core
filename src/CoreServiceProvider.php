78015bdf92451424597b305cc6fe2f2cc3a4d390
Aleksandr Domanskiy <spirit412@gmail.com>
2024-10-08 12:38:54 +0300

    1.0.19

--- src/CoreServiceProvider.php
+++ src/CoreServiceProvider.php
@@ -11,15 +11,21 @@ class CoreServiceProvider extends ServiceProvider
      */
     public function boot(SvrCore $extension)
     {
+        // зарегистрировать переводы
+        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'svr-core-lang');
+        //зарегистрировать шаблоны пакета
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'svr-core');
+
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../database/seeders');
+
         if ($views = $extension->views()) {
             $this->loadViewsFrom($views, 'svr-core');
         }
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'svr-core');
 
         if ($this->app->runningInConsole()) {
             $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
             $this->loadMigrationsFrom(__DIR__.'/../database/seeders');
-            $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'svr-core-lang');
         }
 
         CoreManager::boot();
