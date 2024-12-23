7c33c90a660c168404ecd916e23133aafc6f23dd
Alexander.L <lomovtsev.ab@plinor.ru>
2024-12-23 18:32:02 +0300

    Создание очереди на получение списка хозяйств - II

--- routes/console.php
+++ routes/console.php
@@ -9,6 +9,6 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote')->hourly();
 Schedule::command('inspire')->everyTenMinutes()->runInBackground();
 Schedule::command('route:list')->dailyAt('02:00');
-Schedule::command('queue:work --queue=email')->onevery();
+Schedule::command('queue:work --queue=email')->everyMinute();
 Schedule::command('queue:work --queue=crm')->everyMinute();
 
