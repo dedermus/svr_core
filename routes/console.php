f93cf3ea9ac1113e8e45aa0a944700044148b0d9
Alexander.L <lomovtsev.ab@plinor.ru>
2025-01-17 10:17:53 +0300

    Сведение веток в пятницу с утра

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,14 @@
 <?php
 
-//use Illuminate\Support\Facades\Schedule;
-//
-//Schedule::command('queue:work --queue=crm')->everyMinute()->withoutOverlapping()->description('Получение данных из CRM');
-//Schedule::command('queue:work --queue=email')->everyMinute()->withoutOverlapping()->description('Отправка email уведомлений');
-//Schedule::command('queue:monitor')->everyMinute()->description('Монитор очередей');
+use Illuminate\Support\Facades\Schedule;
+use Svr\Core\Jobs\ProcessImportMilk;
+
+Schedule::job(new ProcessImportMilk, 'import_milk')->everyMinute();
+
+
+Schedule::command('herriot:update-directories')->everyMinute()->runInBackground()->description('Обновление справочников из Хорриота');
+Schedule::command('herriot:update-company')->everyMinute()->runInBackground()->description('Обновление компаний из Хорриота');
+Schedule::command('herriot:update-company-objects')->everyMinute()->runInBackground()->description('Обновление поднадзорных объектов из Хорриота');
+Schedule::command('herriot:send-animals')->everyMinute()->runInBackground()->description('Отправка животных на регистрацию в Хорриот');
+Schedule::command('herriot:check-send-animals')->everyMinute()->runInBackground()->description('Проверка статуса регистрации животных в Хорриот');
 
