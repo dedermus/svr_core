1eb9967070f24328b42640f1187a01fa4a5c69fc
Alexander.L <lomovtsev.ab@plinor.ru>
2025-01-17 11:17:33 +0300

    Сформировали корректно журнал задач

--- routes/console.php
+++ routes/console.php
@@ -1,10 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Schedule;
+use Svr\Core\Jobs\ProcessCrmGetListCountries;
+use Svr\Core\Jobs\ProcessCrmGetListDistricts;
+use Svr\Core\Jobs\ProcessCrmGetListFarms;
+use Svr\Core\Jobs\ProcessCrmGetListRegions;
+use Svr\Core\Jobs\ProcessCrmGetListUsers;
 use Svr\Core\Jobs\ProcessImportMilk;
 
-Schedule::job(new ProcessImportMilk, 'import_milk')->everyMinute();
+Schedule::job(new ProcessImportMilk, 'import_milk')->everyMinute()->description('Импорт животных молочного КРС');
+//Schedule::job(new ProcessImportBeef, 'import_beef')->everyMinute()->description('Импорт животных мясного КРС');
+//Schedule::job(new ProcessImportSheep, 'import_sheep')->everyMinute()->description('Импорт животных (овец) МРС');
 
+Schedule::job(new ProcessCrmGetListCountries, 'crm')->dailyAt('00:10')->description('Обновление справочников стран из CRM');
+Schedule::job(new ProcessCrmGetListRegions, 'crm')->dailyAt('00:30')->description('Обновление справочников регионов/областей из CRM');
+Schedule::job(new ProcessCrmGetListDistricts, 'crm')->dailyAt('00:50')->description('Обновление справочников районов из CRM');
+Schedule::job(new ProcessCrmGetListFarms, 'crm')->dailyAt('01:10')->description('Обновление справочников компаний из CRM');
+Schedule::job(new ProcessCrmGetListUsers, 'crm')->dailyAt('02:30')->description('Обновление справочников пользователей из CRM');
 
 Schedule::command('herriot:update-directories')->everyMinute()->runInBackground()->description('Обновление справочников из Хорриота');
 Schedule::command('herriot:update-company')->everyMinute()->runInBackground()->description('Обновление компаний из Хорриота');
